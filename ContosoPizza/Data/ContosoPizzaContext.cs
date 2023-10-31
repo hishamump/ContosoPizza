ec5b70408a99a7dbed2d85d39abe617e78ed916d
Hisham Alwanni <hisham.alwanny@hotmail.com>
2023-10-31 20:21:15 +0300

    Add project files.

---
+++ ContosoPizza/Data/ContosoPizzaContext.cs
@@ -0,0 +1,24 @@
+﻿using ContosoPizza.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContosoPizza.Data
+{
+    public class ContosoPizzaContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ContosoPizza;Integrated Security=True;");
+        }
+    }
+}
---
+++ ContosoPizza/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ContosoPizza.Models;
+
+Console.WriteLine("Hello, World!");
+
+Customer x = new Customer();
+
+Console.WriteLine($"x = -{x.FirstName.Length}-");
