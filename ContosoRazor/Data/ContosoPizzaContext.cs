62092b612f5283cdfa7c2860ad85d5798272f755
Hisham Alwanni <hisham.alwanny@hotmail.com>
2023-11-03 20:42:33 +0300

    Move connection string to user secrets

--- ContosoRazor/Data/ContosoPizzaContext.cs
+++ ContosoRazor/Data/ContosoPizzaContext.cs
@@ -24,10 +24,6 @@ public partial class ContosoPizzaContext : DbContext
 
     public virtual DbSet<Product> Products { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ContosoPizza;Integrated Security=True;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Order>(entity =>
--- ContosoRazor/Program.cs
+++ ContosoRazor/Program.cs
@@ -1,11 +1,13 @@
 using ContosoRazor.Data;
 using ContosoRazor.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+builder.Services.AddDbContext<ContosoPizzaContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ContosoPizza")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
